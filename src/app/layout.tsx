46d42ab2a4b6d2b566d96c58495a87d91b90ce52
nileshkr022 <nileshsindri2003@gmail.com>
2025-07-09 02:53:51 +0530

    update:Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -26,11 +28,22 @@ export default function RootLayout({
 }>) {
   return (
     <ConvexClerkProvider>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      > <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        <div className="min-h-screen">
+          <Navbar/>
+          <main className="px-4 sm:px-6 lg:px-8">
+            {children}
+            </main>
+        </div>
+        </ThemeProvider>
       </body>
     </html>
     </ConvexClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-10">
-      <SignInButton>
-        <Button>Log in</Button>
-      </SignInButton>
+      Home page
     </div>
   );
 }
