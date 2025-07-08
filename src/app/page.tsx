87cbed6e0f6e50738d8d6bf76e9f0a7a89a8555a
nileshkr022 <nileshsindri2003@gmail.com>
2025-07-08 22:21:31 +0530

    feature:Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>HI</h1>
-      <Button>HI</Button>
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
