fa3ade72491d12bfd69de6606d09f21c58a90c05
nileshkr022 <nileshsindri2003@gmail.com>
2025-10-30 17:13:42 +0530

    login bug fixed

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -30,13 +30,7 @@ const Login = () => {
     });
   };
 
-  // Autofill demo credentials
-  const fillDemoCredentials = () => {
-    setFormData({
-      email: 'khushirice07@gmail.com',
-      password: 'Password',
-    });
-  };
+  
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
